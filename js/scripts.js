7214b4007be5bdf9d24141ed0c4768dcebacf667
Tyler Dix <tyler.dix@gmail.com>
2019-01-06 14:12:40 -0700

    Cleaned up comments in scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,5 @@
 // Toggle nav menu
 
-// let mainNav = document.getElementById('js-menu');
-// let navBarToggle = document.getElementById('js-navbar-toggle');
-
-// navBarToggle.addEventListener('click', function () {
-    
-//     mainNav.classList.toggle('active');
-// });
-
-// $( "#js-navbar-toggle" ).click(function() {
-//   $( "#js-menu" ).slideToggle( "fast", function() {
-//     // Animation complete.
-//   });
-// });
-
 $( document ).ready(function() {
 $( "#js-navbar-toggle" ).click(function() {
   $( "#js-menu" ).slideToggle( "fast", function() {
