034d0eb255a9a7dc341d87583262f45cd4888a3e
Tyler Dix <tyler.dix@gmail.com>
2019-01-13 17:42:18 -0700

    Nearly finished homepage

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,13 @@ $( "#js-navbar-toggle" ).click(function() {
   });
 });
 });
+
+$("a[href^='#']").click(function(e) {
+  e.preventDefault();
+  
+  var position = $($(this).attr("href")).offset().top;
+
+  $("body, html").animate({
+    scrollTop: position
+  } /* speed */ );
+});
