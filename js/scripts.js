778eb25e512e4a733c6c653d05d9802b58414be8
Tyler Dix <tyler.dix@gmail.com>
2019-01-25 10:40:09 -0700

    Rearranged sections. Adjusted colors. Fixed navbar collapsing issue on link click.

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,15 @@
 alert("This website is for testing purposes only. You can find the original website here: https://worldofillumination.com/");
 
 $( document ).ready(function() {
-$( "#js-navbar-toggle" ).click(function() {
+$( "#js-navbar-toggle, .menu-collapse" ).click(function() {
   $( "#js-menu" ).slideToggle( "fast", function() {
     // Animation complete.
   });
 });
 });
+
+
+// scrolly effect
 
 $("a[href^='#']").click(function(e) {
   e.preventDefault();
