e85b70b2e99838a3e9915d3ea929a2fd9a16b571
Tyler Dix <tyler.dix@gmail.com>
2019-01-26 12:25:08 -0700

    Fixed poster image

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,12 @@
-// Toggle nav menu
 
-// let mainNav = document.getElementById('js-menu');
-// let navBarToggle = document.getElementById('js-navbar-toggle');
-
-// navBarToggle.addEventListener('click', function () {
-    
-//     mainNav.classList.toggle('active');
-// });
-
-// $( "#js-navbar-toggle" ).click(function() {
-//   $( "#js-menu" ).slideToggle( "fast", function() {
-//     // Animation complete.
-//   });
-// });
 alert("This website is for testing purposes only. You can find the original website here: https://worldofillumination.com/");
 
-$( document ).ready(function() {
+
 $( "#js-navbar-toggle, .menu-collapse" ).click(function() {
   $( "#js-menu" ).slideToggle( "fast", function() {
     // Animation complete.
   });
 });
-});
-
 
 // scrolly effect
 
